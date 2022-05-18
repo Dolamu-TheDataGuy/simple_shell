1967f3662a3e263630ca693230353dceaa0c54a5
Dolamu Oludare <78808685+Dolamu-TheDataGuy@users.noreply.github.com>
2022-05-18 14:13:42 -0700

    added shell functions

--- shell_funcs.c
+++ shell_funcs.c
@@ -1,11 +1,40 @@
-#include "main.h"
+#include "holberton.h"
 
-void display_prompt(void)
+/**
+ * insertNullByte - inserts null byte at given index
+ * @str: input string
+ * @index: index to insert null byte
+ */
+void insertNullByte(char *str, unsigned int index)
 {
-    write(STDERR_FILENO, "$ ", 2);
+	str[index] = '\0';
 }
 
-void display_newline()
+/**
+ * displayPrompt - displays shell prompt
+ */
+void displayPrompt(void)
 {
-    write(STDERR_FILENO, "\n", 1);
+	write(STDERR_FILENO, "$ ", 2);
+}
+
+/**
+ * displayNewLine - displays new line
+ */
+void displayNewLine(void)
+{
+	write(STDOUT_FILENO, "\n", 1);
+}
+
+/**
+ * sigintHandler - catches SIGINT signal and reset signal
+ * @sigint: signal from stdint
+ */
+void sigintHandler(int sigint)
+{
+	(void)sigint;
+	signal(SIGINT, sigintHandler);
+	displayNewLine();
+	displayPrompt();
+	fflush(stdout);
 }
